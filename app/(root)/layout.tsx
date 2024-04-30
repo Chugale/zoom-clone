c8bc4966198fcbfe17ea5fe6073122ead037362d
Eric Lee <leric7135@gmail.com>
2024-04-30 12:29:14 -0700

    initial commit with basic file structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const RouteLayout = ({ children } : { children: ReactNode }) => {
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
+
+export default RouteLayout
