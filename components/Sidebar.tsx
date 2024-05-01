7e375502a7f77d27b1d9def763295c37c3db9bd6
Eric Lee <leric7135@gmail.com>
2024-05-01 10:13:29 -0700

    web, mobile and side bar navigations

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,14 +4,15 @@ import { sidebarLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 const Sidebar = () => {
   const pathname = usePathname();
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
-      <div className='flex flex- flex-col gap-6'>
+      <div className='flex flex-1 flex-col gap-6'>
         {sidebarLinks.map((link) => {
-          const isActive = pathname === link.route || pathname.startsWith(link.route);
+          const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
           return (
             <Link
               href={link.route}
@@ -20,7 +21,15 @@ const Sidebar = () => {
                 'bg-blue-1': isActive
               })}
             >
-              {link.label}
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className='text-lg font-semibold max-lg:hidden'>
+                {link.label}
+              </p>
             </Link>
           )
         })}
